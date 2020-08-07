ecd03ceda5cf257f8d7368ae816287d00c71b3ab
Eduard Makarichak <makarichak1209@gmail.com>
2020-08-07 16:42:38 +0300

    admin

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('.slider').slick({
+
+	arrows: false,
+	autoplay: true,
+  	autoplaySpeed: 7000,
+	fade: true,
+	infinite: true,
+  	cssEase: 'linear',
+	draggable: false,
+	pauseOnFocus: false,
+	pauseOnHover: false,
+
+  });
+
+  $(function(){
+    //$('#exampleModal').arcticmodal();
+
+});
+});
+
