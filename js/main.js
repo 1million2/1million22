abd0314279b5e2b2c8464d104219d1a2c4916479
Eduard Makarichak <makarichak1209@gmail.com>
2020-10-10 11:10:15 +0300

    add new block in index

--- js/main.js
+++ js/main.js
@@ -29,12 +29,12 @@ let shop = document.querySelectorAll(".shop");
 // показываем блоки если страцица загрузилась посередине и не нужно скролить
 for ( let shopItem of shop){
 	window.addEventListener("DOMContentLoaded", function() {
-		if (pageYOffset >= 400) {
-			shopItem.classList.add("shop-visible");
+		if (pageYOffset >= 350) {
+			shopItem.classList.add("shop-visible");	
 		}
 	})
 }
-// показываем 3 блока с магазинами при скроле на 500px
+// показываем 3 блока с магазинами при скроле на 300px
 function showShopContainer1(scrollPos) {
 	if (pageYOffset >= scrollPos) {
 		setTimeout(
@@ -42,29 +42,33 @@ function showShopContainer1(scrollPos) {
 				shop[i].classList.add("shop-visible");
 	
 				if (++i < (shop.length / 2)) {
-					setTimeout(iter, 300, i);
+					setTimeout(iter, 400, i);
 				}
 			}, 0, 0);
 	}
 }
-// показываем остальные 3 блока на 1450px
+// показываем остальные 3 блока на 1150px
 function showShopContainer2(scrollPos) {
 	if (pageYOffset >= scrollPos ) {
 		setTimeout(
 			function iter(i) {
 				shop[i].classList.add("shop-visible");
 				if (++i < shop.length) {
-					setTimeout(iter, 300, i);
+					setTimeout(iter, 400, i);
 				}
 			}, 0, 3);
 	}
 }
+
 // добавляем слушатель и вешаем на него 2 эти функции для отображения блоков при скроле
 window.addEventListener("scroll", function () {
-
-		showShopContainer1(400);
-		showShopContainer2(1350);	
+	showShopContainer1(400);
+	showShopContainer2(1150);
 });
+
+
+
+
 
 
 
